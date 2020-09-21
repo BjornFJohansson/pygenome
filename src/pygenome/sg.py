5732a3410bf39fb65a2bddf3c559258d353d32a6
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-09-21 15:05:18 +0100

    fix pickle5

--- src/pygenome/sg.py
+++ src/pygenome/sg.py
@@ -4,9 +4,13 @@
    Sequences can be accessed as Bio.SeqRecord objects provided by Biopython.
 '''
 
+import platform as _platform
 
+if _platform.python_version().startswith("3.8"):
+    import pickle        as _pickle
+else:
+    import pickle5       as _pickle
 import os            as _os
-import pickle        as _pickle
 
 data_dir = _os.path.join( _os.getenv("pygenome_data_dir"), "Saccharomyces_cerevisiae")
 
--- src/pygenome/standard_name.py
+++ src/pygenome/standard_name.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import platform as _platform
 
+if _platform.python_version().startswith("3.8"):
+    import pickle        as _pickle
+else:
+    import pickle5       as _pickle
 import os               as _os
 import re               as _re
-import pickle           as _pickle
 from pygenome._pretty   import pretty_str as _ps
 
 data_dir = _os.path.join( _os.getenv("pygenome_data_dir"), "Saccharomyces_cerevisiae")
@@ -12,7 +16,7 @@ _feature_list = _pickle.load( open(_os.path.join(data_dir, "feature_list.pickle"
 
 
 def _standard_name(gene):
-    
+
     gene = gene.upper()
 
     if not _re.match(r"Y[A-P](R|L)\d{3}(W|C)(-.)*", gene[:7]):
