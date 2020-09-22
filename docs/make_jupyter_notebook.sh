1f372bc255e2d2652f930ef59c48ec76af458323
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-09-22 08:11:22 +0100

    start gh action

---
+++ docs/make_jupyter_notebook.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+bash jupytext --to notebook readme_session.py
+
+echo "press any key to close"
+read -n1 slask
--- docs/readme_session.py
+++ docs/readme_session.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# In[]
 from pygenome import sg
+# In[]
 mygene = sg.stdgene["XKS1"]
+# In[]
 mygene
+# In[]
 mygene.short_description
+# In[]
 sg.sysgene["YGR194C"]
+# In[]
 mygene.cds
+# In[]
 mygene.locus()
+# In[]
 mygene.promoter
+# In[]
 mygene.terminator
+# In[]
 mygene.downstream_gene
+# In[]
 mygene.upstream_gene
+# In[]
 mygene.deletion_locus
