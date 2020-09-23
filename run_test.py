67237ce3e1eee6a92f4c7ea7f96f922255359443
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-09-23 18:40:42 +0100

    fix

--- run_test.py
+++ run_test.py
@@ -10,17 +10,6 @@ import pathlib
 import pkg_resources
 
 
-try:
-    from pyfiglet import Figlet
-except ImportError:
-    asciitext = print
-else:
-
-    def asciitext(*args, **kwargs):
-        f = Figlet(font="doom")
-        print(f.renderText(" ".join(args)), **kwargs)
-
-
 def main():
 
     os.environ["pygenome_data_dir"]    = tempfile.mkdtemp(prefix="pygenome_data_dir_")
@@ -28,54 +17,28 @@ def main():
     os.environ["pygenome_config_dir"]  = tempfile.mkdtemp(prefix="pygenome_config_dir_")
     os.environ["pygenome_loglevel"]    = str( logging.DEBUG )
 
-    asciitext("tests py {}".format(platform.python_version()))
-
-    installed = {pkg.key for pkg in pkg_resources.working_set}
-
-    if "pytest-cov" in installed:
-        print("pytest-cov is installed.")
-        args = [
-            "--cov=pygenome",
-            "--cov-report=html",
-            "--cov-report=xml",
-            "--import-mode=importlib",
-        ]
-    else:
-        print("pytest-cov NOT installed! (pip install pytest-cov)")
-        args = []
-
-    if "nbval" in installed:
-        print("nbval is installed.")
-        args.append("--nbval")
-        args.append("--current-env")
-    else:
-        print("nbval NOT installed! (pip install nbval)")
-
-    mainargs = [ "tests",
-                "--capture=no",
-                "--durations=10",
-                "-v"] + args
-
-    result_suite = pytest.cmdline.main(mainargs)
-
-
     from pygenome import __file__ as pygenomeinit
+
     doctestdir = str(pathlib.Path(pygenomeinit).parent)
-    asciitext("doctests py {}".format(platform.python_version()))
 
-    doctestargs = [
-    doctestdir,
-    "--doctest-modules",
-    "--capture=no",
-    "--import-mode=importlib",
-    "--capture=no",
-    "-v"]
+    args = [ "tests",
+             doctestdir,
+             "--capture=no",
+             "--durations=10",
+             "--cov=pygenome",
+             "--cov-report=html",
+             "--cov-report=xml",
+             "--import-mode=importlib",
+             "--nbval",
+             "--current-env",
+             "--doctest-modules",
+             "--capture=no",
+             "--import-mode=importlib",
+             "-vvv"]
 
-    result_doctest = pytest.cmdline.main(doctestargs)
+    result_suite = pytest.cmdline.main(args)
 
-    asciitext("done!")
-
-    return result_doctest and result_suite
+    return result_suite
 
 
 if __name__ == "__main__":
